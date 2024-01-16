1dd42439e7cc67a08e3087dced043b3e29ec02f6
joshuakto <34743132+joshuakto@users.noreply.github.com>
2024-01-16 10:17:18 +0800

    reformatting

--- st_app.py
+++ st_app.py
@@ -7,7 +7,7 @@ import geemap.foliumap as geemap
 import ee
 from streamlit_folium import folium_static
 
-st.write("""# Land Impacted by Rising Sea Level highlighted in red""")
+st.write("# Land Impacted by Rising Sea Level highlighted in red")
 ee.Initialize(ee.ServiceAccountCredentials(
     st.secrets.gee_service_account,
     key_data=st.secrets.gee_service_account_credentials))
@@ -17,13 +17,10 @@ m = geemap.Map(center=(22.30, 114.1694), zoom=14, basemap="HYBRID")
 dem = ee.Image("NASA/NASADEM_HGT/001")
 impacted_land = dem.expression(
     "(elevation < 0.3) && (swb == 0)",
-    {
-        'elevation': dem.select('elevation'),
-        'swb': dem.select('swb')
-    }
-)
-viz_params = {'min': 0, 'max': 1, 'palette': [
-    '000000', 'FF0000'], 'opacity': 0.4}
+    {'elevation': dem.select('elevation'),
+     'swb': dem.select('swb')})
+viz_params = {'min': 0, 'max': 1, 'palette': ['000000', 'FF0000'],
+              'opacity': 0.4}
 
 m.addLayer(impacted_land, viz_params, 'impacted areas')
 m.addLayerControl()
